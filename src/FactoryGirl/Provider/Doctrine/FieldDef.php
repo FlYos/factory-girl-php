b4c0ceec19b6465ebefa039d2e23a15bcde809fc
Steven <steven@webaki.com>
2016-12-19 10:55:40 +0100

    Replace current version of sequence, time by Faker library

--- src/FactoryGirl/Provider/Doctrine/FieldDef.php
+++ src/FactoryGirl/Provider/Doctrine/FieldDef.php
@@ -1,59 +1,13 @@
 <?php
 
 namespace FactoryGirl\Provider\Doctrine;
+use Faker\Factory;
 
 /**
  * Contains static methods to define fields as sequences, references etc.
  */
 class FieldDef
 {
-    /**
-     * Defines a field to be a string based on an incrementing integer.
-     * 
-     * This is typically used to generate unique names such as usernames.
-     * 
-     * The parameter may be a function that receives a counter value
-     * each time the entity is created or it may be a string.
-     * 
-     * If the parameter is a string string containing "%d" then it will be
-     * replaced by the counter value. If the string does not contain "%d"
-     * then the number is simply appended to the parameter.
-     * 
-     * @param callable|string $funcOrString The function or pattern to generate a value from.
-     * @param int $firstNum The first number to use.
-     * @return callable
-     */
-    public static function sequence($funcOrString, $firstNum = 1)
-    {
-        $n = $firstNum - 1;
-        if (is_callable($funcOrString)) {
-            return function() use (&$n, $funcOrString) {
-                $n++;
-                return call_user_func($funcOrString, $n);
-            };
-        } elseif (strpos($funcOrString, '%d') !== false) {
-            return function() use (&$n, $funcOrString) {
-                $n++;
-                return str_replace('%d', $n, $funcOrString);
-            };
-        } else {
-            return function() use (&$n, $funcOrString) {
-                $n++;
-                return $funcOrString . $n;
-            };
-        }
-    }
-
-    public static function past()
-    {
-        return new DateIntervalHelper(new \DateTime(), true);
-    }
-
-    public static function future()
-    {
-        return new DateIntervalHelper(new \DateTime(), false);
-    }
-    
     /**
      * Defines a field to `get()` a named entity from the factory.
      *
@@ -69,5 +23,15 @@ class FieldDef
         return function(FixtureFactory $factory) use ($name) {
             return $factory->get($name);
         };
+    }
+
+    /**
+     * @param string $locale
+     *
+     * @return \Faker\Generator
+     */
+    public static function faker($locale = Factory::DEFAULT_LOCALE)
+    {
+        return Factory::create($locale);
     }
 }
